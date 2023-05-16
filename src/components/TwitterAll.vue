bc0a4dd1bdf78e58cbca690c016a40c09773be95
Joanna Li <joannali@JoannadeMBP.modem>
2023-05-17 01:07:01 +1000

    get data

--- src/components/TwitterAll.vue
+++ src/components/TwitterAll.vue
@@ -2,12 +2,38 @@
     <div>
       <h1>Twitter</h1>
       <p>This is the twitter page content.</p>
+      <button @click="getData">testdata</button>
+      <ul>
+        <li v-for="(item, index) in data" :key="index">{{ item }}</li> 
+      </ul>
     </div>
   </template>
   
   <script>
+  import axios from 'axios';
+
   export default {
     name: 'TwitterAll',
+    props:{
+      msg: String
+    },
+    data() {
+      return{
+        data: [],
+      };
+    },
+    methods: {
+      async getData() {
+        try {
+          const response = await axios.get('http://127.0.0.1:5000/getTest');
+          console.log(response.data);
+          this.data = response.data;
+       
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
   }
   </script>
   
