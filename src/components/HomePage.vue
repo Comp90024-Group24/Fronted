0425f4a1ff8152daedb49c29f183c6bc1f79e80a
Joanna Li <joannali@ravpn-200-student-10-8-1-62.uniaccess.unimelb.edu.au>
2023-05-22 05:09:55 +1000

    almost fin

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,20 @@
 <template>
     <div>
-      <h1>COMP90024 Group 24</h1>
+      <h5>COMP90024 Group 24</h5>
+      <h2>Analysis of the Impact of Twitter Users and Industry in Australia</h2>
       <p>This is the home page content.</p>
+      <img src="@/assets/Unknown.jpeg" alt="description of image 1">
+      <img src="@/assets/Unknown.png" alt="description of image 2">
+      <p>
+      <strong>Group members:</strong>
+      <ul>
+        <li>Kejie Xu 1342103</li>
+        <li>Jiyuan Chen 1323889</li>
+        <li>Jiakang Li 1287117</li>
+        <li>Yuqing Li 1042408</li>
+        <li>Xueqing Li 1340489</li>
+      </ul>
+    </p>
     </div>
   </template>
   
--- src/components/MastodonH.vue
+++ src/components/MastodonH.vue
@@ -1,16 +1,21 @@
 <template>
+  
   <div>
     <h1>Mastodon Data</h1>
     <p>This is the Mastodon page content.</p>
-    <div class="button-container">
-      <button class="btn btn-primary" @click="getData1">job number</button>
-      <button class="btn btn-primary" @click="getData2">job Time</button>
+    
+    <button class="btn btn-primary" @click="getData1">month and num</button>
+      
 
-    </div>
-
-    <button class="btn btn-primary close-button" @click="closeData">close</button>
-
-    <div id="chart" v-show="showChart1" style="width: 600px;height:400px;"></div>
+   
+    <ul class="list-group">
+        <li class="list-group-item" v-for="(item, index) in data" :key="index">
+          {{ item }}
+        </li>
+      </ul>
+    
+    <button class="btn btn-primary sec-button" @click="getData2">polarity</button>
+    
     <div id="chart2" v-show="showChart2" style="width: 600px;height:400px;"></div>
 
 
@@ -23,47 +28,48 @@ import * as echarts from 'echarts';
 
 export default {
   name: 'MastodonH',
+  props: {
+    msg: String
+  },
+
   data() {
     return {
-      chart: null,
+
       chart2: null,
 
 
       data: [],
       data2: [],
 
-
-      showChart1: false,
       showChart2: false,
+
+
+
+
+      
 
 
     };
   },
   async mounted() {
-    this.chart = echarts.init(document.getElementById('chart'));
+
     this.chart2 = echarts.init(document.getElementById('chart2'));
 
   },
   methods: {
-    closeData() {
-      this.showChart1 = false;
-      this.showChart2 = false;
-
-    },
     async getData1() {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/eachjobtype_num');
+        const response = await axios.get('http://127.0.0.1:8000/mastodon_month_num');
         console.log(response.data);
         this.data = response.data;
-        this.showChart1 = true;
-        this.updateChart();
+
       } catch (error) {
         console.log(error);
       }
     },
     async getData2() {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/job_type_time');
+        const response = await axios.get('http://127.0.0.1:8000/polarity_sum');
         console.log(response.data);
         this.data2 = response.data;
         this.showChart2 = true;
@@ -72,85 +78,15 @@ export default {
         console.log(error);
       }
     },
-
-
-
-    updateChart() {
-      let artData = this.data.filter(item => item.Eachjobtype.startsWith('art')).map(item => item.num);
-      let eduData = this.data.filter(item => item.Eachjobtype.startsWith('education')).map(item => item.num);
-      let finData = this.data.filter(item => item.Eachjobtype.startsWith('finance')).map(item => item.num);
-      let techData = this.data.filter(item => item.Eachjobtype.startsWith('technolopy')).map(item => item.num);
-      let totalArt = artData.reduce((a, b) => a + b, 0);
-      let totalEdu = eduData.reduce((a, b) => a + b, 0);
-      let totalFin = finData.reduce((a, b) => a + b, 0);
-      let totalTech = techData.reduce((a, b) => a + b, 0);
-
-      const option = {
-
-        title: {
-          text: 'Job Count by Type'
-        },
-        tooltip: {},
-        xAxis: [{
-          type: 'category',
-          data: ['ART', 'EDU', 'FIN', 'TECH'],
-          axisTick: {
-            alignWithLabel: true
-          },
-          axisLabel: {
-            interval: 0,
-            rotate: 45
-          }
-        }],
-        yAxis: [
-          {
-            type: 'value',
-            name: 'type',
-            min: 0,
-            max: 500000,
-
-          },
-          {
-            type: 'value',
-            name: 'sum',
-
-          }
-        ],
-        series: [
-          {
-            name: 'Labourers',
-            type: 'bar',
-            data: [artData[0], eduData[0], finData[0], techData[0]]
-          },
-          {
-            name: 'Managers',
-            type: 'bar',
-            data: [artData[1], eduData[1], finData[1], techData[1]]
-          },
-          {
-            name: 'Professionals',
-            type: 'bar',
-            data: [artData[2], eduData[2], finData[2], techData[2]]
-          },
-          {
-            name: 'Total',
-            type: 'line',
-            yAxisIndex: 1,
-            data: [totalArt, totalEdu, totalFin, totalTech]
-          }
-        ]
-      };
-      this.chart.setOption(option);
-    },
     updateChart2() {
       const option = {
         title: {
-          text: 'Average Work Time by City'
+          text: 'Polarity Sum'
         },
         tooltip: {},
         xAxis: [{
           type: 'category',
-          data: ['Adelaide', 'Capital', 'Brisbane', 'Darwin', 'Hobart', 'Melbourne', 'Perth', 'Sydney'],
+          data: ['Negative', 'Neutral', 'Positive'],
           axisTick: {
             alignWithLabel: true
           },
@@ -163,9 +99,9 @@ export default {
         yAxis: {
         },
         series: [{
-          name: 'time',
+          name: 'sum',
           type: 'bar',
-          data: this.data2.map(item => item.time)
+          data: this.data2.map(item => item.sum)
         }]
       };
       this.chart2.setOption(option);
@@ -176,12 +112,10 @@ export default {
 </script>
   
 <style scoped>
-.button-container {
-  display: flex;
-  justify-content: space-around;
-}
 
-.close-button {
-  margin-top: 20px;
+
+.sec-button {
+  text-align: center;
+  margin-top: 40px;
 }
 </style>
