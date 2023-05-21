cb6c11c76071efd5eff9e87456efd8d62426cc36
Joanna Li <joannali@ravpn-200-student-10-8-1-62.uniaccess.unimelb.edu.au>
2023-05-21 19:15:27 +1000

    chart

--- src/components/TwitterAll.vue
+++ src/components/TwitterAll.vue
@@ -1,47 +1,75 @@
 <template>
-    <div>
-      <h1>Twitter</h1>
-      <p>This is the twitter page content.</p>
-      <button @click="getData">testdata</button>
-      <ul>
-        <li v-for="(item, index) in data" :key="index">{{ item }}</li> 
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
+  <div>
+    <h1>Twitter</h1>
+    <p>This is the twitter page content.</p>
+    <button class="btn btn-primary" @click="getData">update city number</button>
+    <div id="chart" style="width: 600px;height:400px;"></div>
+  </div>
+</template>
 
-  export default {
-    name: 'TwitterAll',
-    props:{
-      msg: String
-    },
-    data() {
-      return{
-        data: [],
-      };
-    },
-    methods: {
-      async getData() {
-        try {
-          const response = await axios.get('http://127.0.0.1:5000/getTest');
-          console.log(response.data);
-          this.data = response.data;
-       
-        } catch (error) {
-          console.log(error);
-        }
+<script>
+import axios from 'axios';
+import * as echarts from 'echarts';
+
+export default {
+  name: 'TwitterAll',
+  data() {
+    return {
+      chart: null,
+      data: [],
+    };
+  },
+  async mounted() {
+    this.chart = echarts.init(document.getElementById('chart'));
+    await this.getData();
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/city_num');
+        console.log(response.data);
+        this.data = response.data;
+        this.updateChart();
+      } catch (error) {
+        console.log(error);
       }
+    },
+    updateChart() {
+      const option = {
+        title: {
+          text: 'Twitter Count by City'
+        },
+        tooltip: {},
+        xAxis: [{
+            type: 'category',
+            data: ['Adelaide', 'Capital', 'Brisbane', 'Darwin', 'Hobart', 'Melbourne', 'Perth', 'Sydney'],
+            axisTick: {
+              alignWithLabel: true
+            },
+            axisLabel: {
+              interval: 0,   
+              rotate: 45 
+            }
+          }
+        ],
+        yAxis: {},
+        series: [{
+          name: 'Twitter Count',
+          type: 'bar',
+          data: this.data.map(item => item.twitterCount)
+        }]
+      };
+      this.chart.setOption(option);
     }
   }
-  </script>
-  
-  <style scoped>
-  .twitterall {
-    text-align: center;
-    margin-top: 20px;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.twitterall {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
+
   
