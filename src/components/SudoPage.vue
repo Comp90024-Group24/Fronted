4809776b6442eab1114ec2a591b033698d48f840
Joanna Li <joannali@ravpn-266-1-student-10-8-61-209.uniaccess.unimelb.edu.au>
2023-05-22 21:38:22 +1000

    change1

--- src/components/SudoPage.vue
+++ src/components/SudoPage.vue
@@ -4,7 +4,9 @@
     <p>This is the SUDO page content.</p>
     <div class="button-container">
       <button class="btn btn-primary" @click="getData1">job number</button>
-      <button class="btn btn-primary" @click="getData2">job Time</button>
+      <button class="btn btn-primary" @click="getData2">city num</button>
+      <button class="btn btn-primary" @click="getData3">gender</button>
+      <button class="btn btn-primary" @click="getData4">work Time</button>
 
     </div>
 
@@ -15,6 +17,8 @@
 
       <div id="chart" v-show="showChart1" style="width: 600px;height:400px;"></div>
       <div id="chart2" v-show="showChart2" style="width: 600px;height:400px;"></div>
+      <div id="chart3" v-show="showChart3" style="width: 600px;height:400px;"></div>
+      <div id="chart4" v-show="showChart4" style="width: 600px;height:400px;"></div>
 
 
     </div>
@@ -32,14 +36,20 @@ export default {
     return {
       chart: null,
       chart2: null,
+      chart3: null,
+      chart4: null,
 
 
       data: [],
       data2: [],
+      data3: [],
+      data4: [],
 
 
       showChart1: false,
       showChart2: false,
+      showChart3: false,
+      showChart4: false,
 
 
     };
@@ -47,12 +57,16 @@ export default {
   async mounted() {
     this.chart = echarts.init(document.getElementById('chart'));
     this.chart2 = echarts.init(document.getElementById('chart2'));
+    this.chart3 = echarts.init(document.getElementById('chart3'));
+    this.chart4 = echarts.init(document.getElementById('chart4'));
 
   },
   methods: {
     closeData() {
       this.showChart1 = false;
       this.showChart2 = false;
+      this.showChart3 = false;
+      this.showChart4 = false;
 
     },
     async getData1() {
@@ -73,6 +87,28 @@ export default {
         this.data2 = response.data;
         this.showChart2 = true;
         this.updateChart2();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getData3() {
+      try {
+        const response = await axios.get('http://172.26.133.42:8000/job_type_gender');
+        console.log(response.data);
+        this.data3 = response.data;
+        this.showChart3 = true;
+        this.updateChart3();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getData4() {
+      try {
+        const response = await axios.get('http://172.26.133.42:8000/num_jobtype');
+        console.log(response.data);
+        this.data4 = response.data;
+        this.showChart4 = true;
+        this.updateChart4();
       } catch (error) {
         console.log(error);
       }
@@ -175,6 +211,116 @@ export default {
       };
       this.chart2.setOption(option);
     },
+    updateChart3() {
+      const option = {
+        title: {
+          text: 'Different gender number by Job Type'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+
+        xAxis: {
+          type: 'category',
+          data: this.data3.map(item => item.job_type)
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: 'Female',
+            data: this.data3.map(item => item.gender.female),
+            type: 'bar',
+            stack: 'gender',
+          },
+          {
+            name: 'Male',
+            data: this.data3.map(item => item.gender.male),
+            type: 'bar',
+            stack: 'gender',
+          },
+        ]
+      };
+      this.chart3.setOption(option);
+    },
+    
+    updateChart4() {
+      
+
+
+
+
+
+
+      const ageGroups = ["16_24", "25_34", "35_39", "41_48", "49over"];
+      const jobTypes = ["art", "education", "finance", "technology"];
+
+      let chartData = [];
+
+      jobTypes.forEach(job => {
+        let jobDataMale = { name: `${job} Male`, type: 'bar', stack: job, data: [] };
+        let jobDataFemale = { name: `${job} Female`, type: 'bar', stack: job, data: [] };
+
+        ageGroups.forEach(age => {
+          let ageData = this.data4.find(item => item.num === `${job}_${age}`);
+          if (ageData) {
+            jobDataMale.data.push(ageData.jobetype.male);
+            jobDataFemale.data.push(ageData.jobetype.female);
+          } else {
+            jobDataMale.data.push(0);
+            jobDataFemale.data.push(0);
+          }
+        });
+
+        chartData.push(jobDataMale);
+        chartData.push(jobDataFemale);
+      });
+
+
+
+
+
+
+
+
+
+      const option = {
+        title: {
+          text: 'Average Work Time by Job Type and Gender'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { type: 'shadow' }
+        },
+        legend: {
+          data: chartData.map(item => item.name),
+          top: '7%',
+        },
+        grid: {
+          top: '20%',
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'value'
+        },
+        yAxis: {
+          type: 'category',
+          data: ['16-24', '25-34', '35-39', '41-48', '49over']
+        },
+        series: chartData
+      };
+
+      this.chart4.setOption(option);
+    },
+    
+
 
   }
 }
@@ -192,6 +338,7 @@ export default {
 
 .chart-container {
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-around;
 }
 </style>
